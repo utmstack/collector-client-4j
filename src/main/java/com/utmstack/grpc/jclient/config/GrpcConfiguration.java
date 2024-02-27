f7def81084b3dff03a65530f03011c9ce32d2e17
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2024-02-27 11:13:13 +0200

    Adding protobuf definitions

---
+++ src/main/java/com/utmstack/grpc/jclient/config/GrpcConfiguration.java
@@ -0,0 +1,42 @@
+package com.utmstack.grpc.jclient.config;
+
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import javax.annotation.PreDestroy;
+
+
+public class GrpcConfiguration {
+    private ManagedChannel channel;
+
+    private String serverAddress;
+
+    private Integer serverPort;
+
+    public GrpcConfiguration() {}
+
+    public GrpcConfiguration withServerAddress(String serverAddress) {
+        this.serverAddress = serverAddress;
+        return this;
+    }
+
+    public GrpcConfiguration withServerPort(Integer serverPort) {
+        this.serverPort = serverPort;
+        return this;
+    }
+
+    public ManagedChannel managedChannel() {
+        this.channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort)
+            .intercept(new GrpcInterceptor())
+            .usePlaintext()
+            .enableRetry()
+            .build();
+        return this.channel;
+    }
+
+    @PreDestroy
+    public void shutdownChannel() {
+        this.channel.shutdown();
+    }
+}
