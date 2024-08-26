b9c7deaa7227126dd6b5a65915384437fd99cf85
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2024-08-26 18:04:26 +0300

    Updating grpc services with new proto changes, adding format error method to improve the code.

--- src/main/java/com/utmstack/grpc/util/StringUtil.java
+++ src/main/java/com/utmstack/grpc/util/StringUtil.java
@@ -1,7 +1,5 @@
 package com.utmstack.grpc.util;
 
-import com.utmstack.grpc.jclient.config.Constants;
-
 /**
  * @author Freddy R. Laffita Almaguer.
  * This class handle useful functions related to String operations
@@ -10,5 +8,11 @@ public class StringUtil {
 
     public static boolean hasText(String str) {
         return (str != null && !str.trim().isEmpty());
+    }
+
+    public static String formatError(String error) {
+        if (hasText(error)){
+            return error.replace("UNAVAILABLE: io exception","SERVER UNAVAILABLE");
+        } else return "";
     }
 }
